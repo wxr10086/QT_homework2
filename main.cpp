9111cb85ca2ef329032f9a68ad5057fcca29036c
wxr10086 <2649740852@qq.com>
2018-10-13 21:21:53 +0800

    加入一个输出列表的函数

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
-#include <iostream>
 #include <QCoreApplication>
-#include<QDebug>
-#include<QVector>
-#include<QList>
+#include <iostream>
+#include <QDebug>
+
 using namespace std;
+
 typedef class{
 public:
     int num;
-    QString name;
+    string name;
     int core1;
     int core2;
 }stu;
-
+void display(QVector<stu> *data)
+{
+    cout<<"Ñ§ºÅ"<<"          "<<"ĞÕÃû"<<"     "<<"¿Î³Ì1"<<"   "<<"¿Î³Ì2"<<endl;
+    for(QVector<stu>::iterator m=data->begin();m!=data->end();m++)
+    {
+        cout<<m->num<<"    "<<m->name<<"     "<<m->core1<<"    "<<m->core2<<endl;
+        //qDebug<<*m<<"\0";
+    }
+    cout<<endl;
+}
 bool c_name(stu a,stu b)
 {
-   if(a.name>=b.name)
-   {
-       return 0;
-   }
-   else
-   {
-       return 1;
-   }
+    if(a.name>=b.name)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
-
 bool c_core1(stu a,stu b)
 {
     if(a.core1>=b.core1)
@@ -35,8 +43,6 @@ bool c_core1(stu a,stu b)
         return 1;
     }
 }
-
-
 bool c_core2(stu a,stu b)
 {
     if(a.core2>=b.core2)
@@ -54,64 +60,43 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     stu stu1,stu2,stu3,stu4;
     stu1.num=1403130209;
-    stu1.name="é²æ™ºæ·±";
+    stu1.name="Â³ÖÇÉî";
     stu1.core1=80;
     stu1.core2=72;
 
     stu2.num=1403140101;
-    stu2.name="æ—å†²  ";
+    stu2.name="ÁÖ³å  ";
     stu2.core1=82;
     stu2.core2=76;
 
     stu3.num=1403140102;
-    stu3.name="å®‹æ±Ÿ  ";
+    stu3.name="ËÎ½­  ";
     stu3.core1=76;
     stu3.core2=85;
 
-    stu4.num=1403140103;
-    stu4.name="æ­¦æ¾  ";
+    stu4.num=140314010;
+    stu4.name=" ÎäËÉ  ";
     stu4.core1=88;
     stu4.core2=80;
-    QList<stu> data;
-    data<<stu1<<stu2<<stu3<<stu4;
-    qDebug("åŸè¡¨æ ¼ä¸º\n");
+    QVector<stu> data;
+    data.push_back(stu1);
+    data.push_back(stu2);
+    data.push_back(stu3);
+    data.push_back(stu4);
+    cout<<"³õÊ¼ÁĞ±íÎª£º\n";
+    display(&data);
+    QVector<stu> s_name=data;
+    sort(s_name.begin(),s_name.end(),c_name);
+    cout<<"°´ĞÕÃûÅÅĞò"<<endl;
+    display(&s_name);
+    QVector<stu> s_core1=data;
+    sort(s_core1.begin(),s_core1.end(),c_core1);
+    cout<<"°´¿Î³Ì1ÅÅĞò"<<endl;
+    display(&s_core1);
+    QVector<stu> s_core2=data;
+    sort(s_core2.begin(),s_core2.end(),c_core2);
+    cout<<"°´¿Î³Ì2ÅÅĞò"<<endl;
+    display(&s_core2);
 
-    for(int i=0;i<4;i++)
-    {
-        qDebug()<<data.at(i).num<<"\t"<<data.at(i).name<<"\t"<<data.at(i).core1<<"\t"<<data.at(i).core2;
-    }
-    qDebug("\n");
-
-
-
-    std:: sort(data.begin(),data.end(),c_name);
-    qDebug("æŒ‰å§“åæ’åº\n");
-    for(int i=0;i<4;i++)
-    {
-        qDebug()<<data.at(i).num<<"\t"<<data.at(i).name<<"\t"<<data.at(i).core1<<"\t"<<data.at(i).core2;
-    }
-        qDebug("\n");
-
-
-    //QList<stu> s_core1=data;
-    std::sort(data.begin(),data.end(),c_core1);
-    qDebug("æŒ‰è¯¾ç¨‹ä¸€æ’åº\n");
-
-    for(int i=0;i<4;i++)
-    {
-        qDebug()<<data.at(i).num<<"\t"<<data.at(i).name<<"\t"<<data.at(i).core1<<"\t"<<data.at(i).core2;
-    }
-        qDebug("\n");
-
-
-
-    //QList<stu> s_core2=data;
-    std:: sort(data.begin(),data.end(),c_core2);
-    qDebug("æŒ‰è¯¾ç¨‹äºŒæ’åº\n");
-    //display(&s_core2);
-    for(int i=0;i<4;i++)
-    {
-        qDebug()<<data.at(i).num<<"\t"<<data.at(i).name<<"\t"<<data.at(i).core1<<"\t"<<data.at(i).core2;
-    }
     return a.exec();
 }
